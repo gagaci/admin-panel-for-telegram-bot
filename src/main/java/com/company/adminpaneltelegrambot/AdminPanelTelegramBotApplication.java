bd68080e9458d5af9969e2b46529f05fa69656f8
khabib <bayramovxabibulloh@gmail.com>
2024-11-01 17:00:30 +0500

    first commit

---
+++ src/main/java/com/company/adminpaneltelegrambot/AdminPanelTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.company.adminpaneltelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPanelTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminPanelTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/adminpaneltelegrambot/AdminPanelTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.adminpaneltelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPanelTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
